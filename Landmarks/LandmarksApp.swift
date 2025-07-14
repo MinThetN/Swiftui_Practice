55fe9995f16a44e6baa37cb0d60a5a1f40ae13fc
MinThetN <minthetn15@gmail.com>
2025-07-14 10:04:47 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Min Thet Naung on 13/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
