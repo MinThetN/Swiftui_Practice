a94b37da3baf44604a5133476c6cce8e2f1179c1
MinThetN <minthetn15@gmail.com>
2025-07-14 15:04:07 +0700

    update

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Min Thet Naung on 14/07/2025.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
