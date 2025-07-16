bc9ec9e265dee21748027510300c14b1190e0497
MinThetN <minthetn15@gmail.com>
2025-07-16 17:08:24 +0700

    update

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     
+    @Environment(ModelData.self) var modelData
+    
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id})!
+    }
+    
     var body: some View {
+        
+        @Bindable var modelData = modelData
         
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
@@ -22,8 +30,11 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.largeTitle)
+                HStack {
+                    Text(landmark.name)
+                        .font(.largeTitle)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
@@ -49,5 +60,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
